d29c3725b90233b31775d966c201afff1ceee368
LengYXin <lengyingxin8966@gmail.com>
2019-06-03 16:55:50 +0800

    端口检查

--- packages/@samsundot/cli/bin/index.js
+++ packages/@samsundot/cli/bin/index.js
@@ -1,2 +1,39 @@
 #!/usr/bin/env node
-console.log("测试")
+const program = require('commander')
+program
+    .version(require('../package').version)
+    .usage('<command> [options]');
+
+program
+    .command('create <app-name>')
+    .description('创建新的项目')
+    .action((name, cmd) => {
+        const options = cleanArgs(cmd)
+        require('../lib/create')(name, options);
+    });
+program
+    .command('server [options]')
+    .description('启动服务')
+    .option('-p, --port <port>', '替换默认端口')
+    .action((event, cmd) => {
+        const options = cleanArgs(cmd)
+        require('../lib/server')(options);
+    });
+program
+    .parse(process.argv);
+// console.log(program.commands)
+function cleanArgs(cmd) {
+    const args = {}
+    cmd.options.forEach(o => {
+        const key = camelize(o.long.replace(/^--/, ''))
+        // if an option is not present and Command has a method with the same name
+        // it should not be copied
+        if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+            args[key] = cmd[key]
+        }
+    })
+    return args
+}
+function camelize(str) {
+    return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
---
+++ packages/@samsundot/cli/lib/server.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer');
+const semver = require('semver');
+const detect = require('detect-port');
+module.exports = (options) => {
+    options = { port: 8000, ...options };
+    const port = options.port;
+    // console.log("server: options", options)
+    detect(port, (err, _port) => {
+        if (err) {
+            console.log(err);
+        }
+        // if (port == _port) {
+        //     console.log(`port: ${port} was not occupied`);
+        // } else {
+        //     console.log(`port: ${port} was occupied, try port: ${_port}`);
+        // }
+        console.log("TCL: _port", _port)
+    });
+}
+
